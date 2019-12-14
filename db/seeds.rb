704f3a6ab826f151bb288e8b54a57b631cc5b852
Lesley Burdock <burdocklesley@gmail.com>
2019-12-14 16:19:51 +0100

    views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Flat.destroy_all
+
+10.times do |flat|
+  Flat.create!(
+    name: 'Light and Bright',
+    address: Faker::Address.full_address,
+    description: 'a pretty and warm home with lots of space and comfort',
+    price_per_night: Faker::Number.decimal(l_digits: 2),
+    number_of_guests: Faker::Number.between(from: 1, to: 5),
+    pic_url: Faker::LoremFlickr.image
+    )
+end
