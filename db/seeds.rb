a00a608a0f83cefe67828e57aa9046b5989cd86a
Lesley Burdock <lesleyburdock@Lesleys-MacBook-Air.local>
2023-05-10 12:05:57 +0200

    3Imagecarousel

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,9 @@ flats = Flat.create(
     description: 'a pretty and warm home with lots of space and comfort',
     price_per_night: Faker::Number.decimal(l_digits: 2),
     number_of_guests: Faker::Number.between(from: 1, to: 5),
-    pic_url: "https://images.unsplash.com/photo-1586023492125-27b2c045efd7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1558&q=80"
+    pic_url: ["https://images.unsplash.com/photo-1618220179428-22790b461013?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3027&q=80",
+      "https://images.pexels.com/photos/2251247/pexels-photo-2251247.jpeg?auto=compress&cs=tinysrgb&w=1200",
+      "https://images.pexels.com/photos/1571460/pexels-photo-1571460.jpeg?auto=compress&cs=tinysrgb&w=1200"]
   )
 
   Flat.create( 
@@ -25,45 +27,62 @@ flats = Flat.create(
     description: 'Green field and beautiful animals. Comfotable home with 5 beds', 
     price_per_night: Faker::Number.decimal(l_digits: 4), 
     number_of_guests: Faker::Number.between(from: 1, to: 5), 
-    pic_url: "https://images.unsplash.com/photo-1618220179428-22790b461013?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3027&q=80"
+    pic_url: ["https://images.pexels.com/photos/1438248/pexels-photo-1438248.jpeg?auto=compress&cs=tinysrgb&w=1200",
+      "https://images.pexels.com/photos/1080696/pexels-photo-1080696.jpeg?auto=compress&cs=tinysrgb&w=1200",
+      "https://images.pexels.com/photos/5644353/pexels-photo-5644353.jpeg?auto=compress&cs=tinysrgb&w=1200"]
   )
-
   Flat.create(
     name: 'Outdoor oasis',
     address: Faker::Address.full_address,
     description: 'a pretty and warm home with lots of space and comfort',
     price_per_night: Faker::Number.decimal(l_digits: 3),
     number_of_guests: Faker::Number.between(from: 1, to: 5),
-    pic_url: "https://images.unsplash.com/photo-1621506821957-1b50ab7787a4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80"
+    pic_url: ["https://images.pexels.com/photos/2980955/pexels-photo-2980955.jpeg?auto=compress&cs=tinysrgb&w=1200",
+      "https://images.pexels.com/photos/447592/pexels-photo-447592.jpeg?auto=compress&cs=tinysrgb&w=1200",
+      "https://images.pexels.com/photos/4352247/pexels-photo-4352247.jpeg?auto=compress&cs=tinysrgb&w=1200"]
     )
     Flat.create( 
       name: 'Family Home', address: Faker::Address.full_address, description: 'Green field and beautiful animals. Comfortable home with 5 beds', 
       price_per_night: Faker::Number.decimal(l_digits: 2), number_of_guests: Faker::Number.between(from: 1, to: 5), 
-      pic_url: "https://images.unsplash.com/photo-1618220179428-22790b461013?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3027&q=80"
+      pic_url: ["https://images.pexels.com/photos/1862402/pexels-photo-1862402.jpeg?auto=compress&cs=tinysrgb&w=1200",
+        "https://images.pexels.com/photos/1648768/pexels-photo-1648768.jpeg?auto=compress&cs=tinysrgb&w=1200",
+        "https://images.pexels.com/photos/1034584/pexels-photo-1034584.jpeg?auto=compress&cs=tinysrgb&w=1200"]
     )
     Flat.create( 
       name: 'Modern Luxury', 
       address: Faker::Address.full_address, 
       description: 'Green field and beautiful animals. Comfotable home with 5 beds', 
-      price_per_night: Faker::Number.decimal(l_digits: 3), number_of_guests: 
-      Faker::Number.between(from: 1, to: 5), 
-      pic_url: "https://images.unsplash.com/photo-1600210492493-0946911123ea?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80")
-    Flat.create(
+      price_per_night: Faker::Number.decimal(l_digits: 3), 
+      number_of_guests: Faker::Number.between(from: 1, to: 5), 
+      pic_url:["https://images.pexels.com/photos/2480608/pexels-photo-2480608.jpeg?auto=compress&cs=tinysrgb&w=1200",
+        "https://images.pexels.com/photos/15201943/pexels-photo-15201943.jpeg?auto=compress&cs=tinysrgb&w=1200",
+        "https://images.pexels.com/photos/261124/pexels-photo-261124.jpeg?auto=compress&cs=tinysrgb&w=1200"])
+    
+      Flat.create(
       name: 'Country Home',
       address: Faker::Address.full_address,
       description: 'Green field and beautiful animals. Comfortable home with 5 beds',
       price_per_night: Faker::Number.decimal(l_digits: 2),
       number_of_guests: Faker::Number.between(from: 1, to: 5),
-      pic_url: "https://images.unsplash.com/photo-1570129477492-45c003edd2be?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+      pic_url:["https://images.pexels.com/photos/1795508/pexels-photo-1795508.jpeg?auto=compress&cs=tinysrgb&w=1200",
+        "https://images.pexels.com/photos/13945431/pexels-photo-13945431.jpeg?auto=compress&cs=tinysrgb&w=1200",
+        "https://images.pexels.com/photos/4617762/pexels-photo-4617762.jpeg?auto=compress&cs=tinysrgb&w=1200"]
     )
-    Flat.create( name: 'Country Home', address: Faker::Address.full_address, description: 'Green field and beautiful animals. Comfotable home with 5 beds', price_per_night: Faker::Number.decimal(l_digits: 4), number_of_guests: Faker::Number.between(from: 1, to: 5), pic_url: "https://images.unsplash.com/photo-1570129477492-45c003edd2be?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80")
+    Flat.create( name: 'Country Home', address: Faker::Address.full_address, description: 'Green field and beautiful animals. Comfotable home with 5 beds', 
+      price_per_night: Faker::Number.decimal(l_digits: 4), 
+      number_of_guests: Faker::Number.between(from: 1, to: 5), 
+      pic_url: ["https://images.pexels.com/photos/314937/pexels-photo-314937.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        "https://images.unsplash.com/photo-1586023492125-27b2c045efd7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1558&q=80",
+        "https://images.unsplash.com/photo-1621506821957-1b50ab7787a4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80"])
     Flat.create(
       name: 'Island Beach house',
       address: Faker::Address.full_address,
       description: 'right on the beach with luxurious fittings',
       price_per_night: Faker::Number.decimal(l_digits: 3),
       number_of_guests: Faker::Number.between(from: 1, to: 5),
-      pic_url: "https://images.unsplash.com/photo-1499793983690-e29da59ef1c2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+      pic_url:["https://images.pexels.com/photos/2128329/pexels-photo-2128329.jpeg?auto=compress&cs=tinysrgb&w=1200",
+        "https://images.pexels.com/photos/12051126/pexels-photo-12051126.jpeg?auto=compress&cs=tinysrgb&w=1200",
+        "https://images.pexels.com/photos/5439496/pexels-photo-5439496.jpeg?auto=compress&cs=tinysrgb&w=1200"]
       )
       Flat.create(
         name: 'Scandinavian comfort',
@@ -71,5 +90,7 @@ flats = Flat.create(
         description: 'traditional home with style. 5 bathrooms',
         price_per_night: Faker::Number.decimal(l_digits: 2),
         number_of_guests: Faker::Number.between(from: 1, to: 5),
-        pic_url: "https://images.unsplash.com/photo-1480074568708-e7b720bb3f09?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2074&q=80"
+        pic_url:["https://images.pexels.com/photos/1212053/pexels-photo-1212053.jpeg?auto=compress&cs=tinysrgb&w=1200",
+          "https://images.pexels.com/photos/7404941/pexels-photo-7404941.jpeg?auto=compress&cs=tinysrgb&w=1200",
+          "https://images.pexels.com/photos/2724749/pexels-photo-2724749.jpeg?auto=compress&cs=tinysrgb&w=1200"]
         )
--- rails-simple-airbnb-app/src/components/carousel.js
+++ rails-simple-airbnb-app/src/components/carousel.js
@@ -1,37 +1,29 @@
-import React from "react";
-import {Carousel} from "flowbite-react";
+// import React from "react";
+// import {Carousel} from "flowbite-react";
 
-const CardCarousel = (props) => {
-  console.log(props.flatPic);
-  return (
-    <div className="h-56 w-full object-fill object-center sm:h-64 xl:h-72 2xl:h-96">
-    <Carousel
-      leftControl=""
-      rightControl=""
-    >
-      <img
-        src={props.flatPic}
-        alt="pic1"
-      />
-      <img
-         src={props.flatPic}
-         alt="pic1"
-      />
-      <img
-         src={props.flatPic}
-         alt="pic1"
-      />
-      <img
-         src={props.flatPic}
-         alt="pic1"
-      />
-      <img
-         src={props.flatPic}
-         alt="pic1"
-      />
-    </Carousel>
-  </div>
-  );
-};
+// const CardCarousel = (props) => {
+//   const { id, flatPic } = props;
+//   // console.log(flatPic)
+//   // console.log(id)
+//  return (
 
-export default CardCarousel;
+//     <div className="h-56 w-full object-fill object-center sm:h-64 xl:h-72 2xl:h-96">
+//     <Carousel slide={false}>
+//       <img
+//         src={flatPic[1]}
+//         alt="pic1"
+//       />
+//       <img
+//          src={flatPic[2]}
+//          alt="pic2"
+//       />
+//       <img
+//          src={flatPic[3]}
+//          alt="pic3"
+//       />
+//     </Carousel>
+//   </div>
+//   );
+// };
+
+// export default CardCarousel;
