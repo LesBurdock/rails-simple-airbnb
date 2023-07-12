551028754991ddbd7a11960316098f4e55326a14
Lesley Burdock <lesleyburdock@Lesleys-MacBook-Air.local>
2023-07-12 12:27:39 +0200

    changes migration table

--- db/migrate/20230509173416_change_photos_column_in_flats.rb
+++ db/migrate/20230509173416_change_photos_column_in_flats.rb
@@ -1,5 +1,11 @@
 class ChangePhotosColumnInFlats < ActiveRecord::Migration[7.0]
   def change
-    change_column :flats, :pic_url, :jsonb, using: 'pic_url::jsonb[]', default: [], '[] USING CAST(pic_url AS jsonb[])'
+    change_column :flats, :pic_url, :jsonb, using: 'pic_url::jsonb[]'
   end
 end
+
+# class ChangePhotosColumnInFlats < ActiveRecord::Migration[7.0]
+#   def change
+#     change_column :flats, :pic_url, :jsonb, using: 'pic_url::jsonb[]', default: [], '[] USING CAST(pic_url AS jsonb[])'
+#   end
+# end
