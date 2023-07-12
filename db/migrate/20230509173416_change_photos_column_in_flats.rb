34b9b61c97ebb50d62e19c0bc77a56614feae8c5
Lesley Burdock <lesleyburdock@Lesleys-MacBook-Air.local>
2023-07-12 11:57:34 +0200

    changes migration file to using cast

--- db/migrate/20230509173416_change_photos_column_in_flats.rb
+++ db/migrate/20230509173416_change_photos_column_in_flats.rb
@@ -1,5 +1,5 @@
 class ChangePhotosColumnInFlats < ActiveRecord::Migration[7.0]
   def change
-    change_column :flats, :pic_url, :jsonb, using: 'pic_url::jsonb[]', default: []
+    change_column :flats, :pic_url, :jsonb, using: 'pic_url::jsonb[]', default: [], '[] USING CAST(pic_url AS jsonb[])'
   end
 end
