f70f6380ad295e2213cb5f2963f26f604023e20c
Lesley Burdock <lesleyburdock@lesleys-air.speedport.ip>
2023-08-17 14:48:47 +0200

    added fallback controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   scope '/api/version1' do
   resources :flats
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
   end
 end
